27398bae17f04e0d8d9f2f20abbdca4c7e2df4d2
skyczhao <chenzhao.me@gmail.com>
2019-06-07 17:34:59 +0800

    common utils

---
+++ utils/excels.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import time
+import pandas as pd
+
+from utils.timer import timer
+
+
+@timer
+def save_excel(sheet, path):
+    """
+    保存dataframe到excel文件
+
+    # excel最大行列数总结:
+    # 在Excel 2010和Excel 2007中, 工作表的大小为: 16,384 列 x 1,048,576 行
+    # 在Excel 97-2003 中, 工作表的大小为: 256 列 x 65,536 行
+    """
+    sheet_shape = sheet.shape
+    print("save size: %s" % str(sheet_shape))
+    if sheet_shape[0] > 1048576:
+        print("row overflow")
+        return
+    if sheet_shape[1] > 16384:
+        print("column overflow")
+        return
+
+    writer = pd.ExcelWriter(path)
+    sheet.to_excel(writer, 'Sheet1')
+    writer.save()
+
+
+@timer
+def each_row(sheet, parser, step=100):
+    """
+    表格行处理
+    """
+    idx = 0
+    for key, row in sheet.iterrows():
+        flag = parser(idx, key, row)
+        if not flag:
+            break
+
+        idx += 1
+        if idx % step == 0:
+            print("current time: %f" % time.time())
+            print("current idx: %d" % idx)
+
---
+++ utils/timer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import time
+
+def timer(f):
+    """
+    简单计时器
+    打印开始时间/结束时间, 并计时
+    """
+    def wrapper(*args, **kargs):
+        start_time = time.time()
+        print("# start at %.5f" % start_time)
+
+        try:
+            return f(*args, **kargs)
+        finally:
+            end_time = time.time()
+            print("# end at %.5f" % end_time)
+            print("total cost: %.5f" % (end_time - start_time))
+    return wrapper
+
